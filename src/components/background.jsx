5be125961170d5ebc5928a1192f210acff74a300
Justin Soberano <jsoberano2002@gmail.com>
2023-05-27 21:28:58 -0500

    Added coming soon

--- src/components/background.jsx
+++ src/components/background.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { Canvas } from "@react-three/fiber";
-import { Stars } from "@react-three/drei";
+import { Html } from "@react-three/drei";
 import { LetterI, LetterJ, LetterU, LetterS, LetterT, LetterN } from "../meshes/letterMeshes";
 
 export default function Background() {
     return (
         <Canvas>
-            <Stars />
-            <gridHelper rotation={[Math.PI/2, 0, 0]} args={[100, 100]} />
             <color attach={"background"} args={["rgb(245, 245, 247)"]} />
             <ambientLight intensity={0.5} />
             <spotLight position={[10, 15, 10]} angle={0.3} />
@@ -17,6 +15,13 @@ export default function Background() {
             <LetterT />
             <LetterI />
             <LetterN />
+            <Html>
+                <nav>
+                    <p>
+                        COMING SOON
+                    </p>
+                </nav>
+            </Html>
         </Canvas>
     );  
 }
