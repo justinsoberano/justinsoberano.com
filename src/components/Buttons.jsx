9b7d1909900a95ffec558f1f64fb0d95fc0746fe
System <system@noreply.github.com>
2023-06-24 20:22:30 -0500

    updates

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,36 +1,46 @@
 import React, { useState } from "react";
-import {useTrail, useSpring , a} from "@react-spring/web";
+import {useSpring , a} from "@react-spring/web";
 import "./styles.css";
+import { Experience } from "./Cards";
 
 /* Try to use maps to create the buttons */
 /* Use 'useTrail' to animate the buttons */
 
 export default function Buttons() {
-
-    function Springs(delay) {
+    
+    function ButtonSprings(delay) {
         return useSpring({
-            from: { opacity: 0, transform: "translateY(100px)" },
-            to: { opacity: 1, transform: "translateY(0px)" },
-            delay: delay,
-            config: { mass: 2, tension: 200, friction: 50, },
+            from: toggle ? { opacity: 0, transform: "translateY(100px)" } : { opacity: 1, transform: "translateY(0px)" },
+            to: toggle ? { opacity: 1, transform: "translateY(0px)" } : { opacity: 0, transform: "translateY(100px)"},
+            delay: toggle ? delay : 0,
+            config: toggle ? { mass: 2, tension: 200, friction: 50 } : { mass: 10, tension: 1000, friction: 75 }
         })
     }
 
+    const [toggle, set] = useState(true);
+
     return(
-        <div className="flex-container">
-            <a.div className="button-design noselect yellow" style={{...Springs(2250)}}>
-                <span className="button-text">experiences</span>
+        <div>
+            <a.div className="flex-container" style={{ ...ButtonSprings(2250)}}>
+                <a.div className="button-design noselect yellow" onClick={() => set(!toggle)} style={{...ButtonSprings(2250)}}>
+                    <span className="button-text">experiences</span>
+                </a.div>
+            <a.div className="button-design noselect aqua" onClick={() => set(!toggle)} style={{...ButtonSprings(2300)}}>
+                    <span className="button-text">projects</span>
+                </a.div>
+            <a.div className="button-design noselect coral" onClick={() => set(!toggle)} style={{ ...ButtonSprings(2400)}}>
+                    <span className="button-text">involvement</span>
+                </a.div>
+            <a.div className="button-design noselect pink" onClick={() => set(!toggle)} style={{ ...ButtonSprings(2450)}}>
+                    <span className="button-text">contact</span>
+                </a.div>
             </a.div>
-            <a.div className="button-design noselect aqua" style={{...Springs(2300)}}>
-                <span className="button-text">projects</span>
-            </a.div>
-            <a.div className="button-design noselect coral" style={{ ...Springs(2400)}}>
-                <span className="button-text">involvement</span>
-            </a.div>
-            <a.div className="button-design noselect pink" style={{ ...Springs(2450)}}>
-                <span className="button-text">contact</span>
-            </a.div>
+            
+            <Experience 
+                show = {toggle}
+            />
         </div>
+
     );
 }
 
---
+++ src/components/Cards.jsx
@@ -0,0 +1,53 @@
+import React, {useEffect, useState} from "react";
+import {useSpring , a} from "@react-spring/web";
+import "./cardstyle.css"
+
+
+export function Experience(toggle) {
+
+    const [show, setShow] = useState(false);
+    useEffect(() => { setShow(!toggle) }, !toggle)
+
+    function CardSpring() {
+        return useSpring({
+            from: show ? { opacity: 0, transform: "translateY(400px)" } : { opacity: 1, transform: "translateY(0px)" },
+            to: show ? { opacity: 1, transform: "translateY(0px)" } : { opacity: 0, transform: "translateY(400px)"},
+            delay: 0,
+            config: { mass: 2, tension: 200, friction: 50 }
+        })
+    }
+
+    return (
+        <>
+            <a.div className="container" style={{...CardSpring()}} key={toggle}>
+                <div className="card-design">
+                    <img className="image" src="https://picsum.photos/450/150"/>
+                    <h1 className="employer">NASA</h1>
+                    <p className="job-dates"> JUN '23 - AUG '23 </p>
+                    <p className="job-title">AI/ML - Computer Vision Intern</p>
+                    <p className="job-description">Developed a image-to-3D AI model that uses depth analysis,
+                                                   image view synthesis, and a bunch of other cool algorithms.
+                                                   A super cool experience!</p>
+                </div>
+                <div className="card-design">
+                    <img className="image" src="https://picsum.photos/450/150"/>
+                    <h1 className="employer">NASA</h1>
+                    <p className="job-dates"> AUG '22 - DEC '22 </p>
+                    <p className="job-title">XR/VR - Digital Twins Intern</p>
+                    <p className="job-description">Worked in a team of four to help design and develop a virtual Mars
+                                                   environment. These environments will be used to train future astronauts
+                                                   for future Mars exploration! </p>
+                </div>
+                <div className="card-design">
+                    <img className="image" src="https://picsum.photos/450/150"/>
+                    <h1 className="employer">ISU IT Dept.</h1>
+                    <p className="job-dates"> AUG '22 - DEC '22 </p>
+                    <p className="job-title">Technical Assistant II</p>
+                    <p className="job-description">Worked in a team of 10 to help service administration level machines
+                                                   throughout campus. Helped other departments order IT equiment when 
+                                                   needed.</p>
+                </div>
+            </a.div>
+        </>
+    )
+}
