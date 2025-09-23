5e81341d0866753bfc464248932d3d6016304ec6
justin soberano <jsoberano2002@gmail.com>
2025-09-23 13:11:09 -0500

    setup new portfolio

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import Background from "./components/canvas/Canvas";
-import { ColorProvider } from "./context/ColorContext";
+import logo from './logo.svg';
+import './App.css';
 
-export default function App() {
+function App() {
   return (
-    <ColorProvider>
-      <Background />
-    </ColorProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { GlobalStyles } from "./components/content/styles/GlobalStyles";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const rootElement = document.getElementById("root");
-if (!rootElement) throw new Error("Failed to find the root element");
-
-const root = ReactDOM.createRoot(rootElement);
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<React.StrictMode>
-		<div style={{ width: "100svw", height: "100svh"}}>
-			<GlobalStyles/>
-			<App/>
-		</div>
-	</React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
