783fa67d332ec8279de0651e04438db9ef0991a6
justin soberano <jsoberano2002@gmail.com>
2025-11-22 10:26:13 -0600

    feat: implement and integrate a new 3D prism background animation component.

--- src/App.js
+++ src/App.js
@@ -5,17 +5,31 @@ import Experience from './components/core/experience/Experience';
 import Footer from './components/core/footer/Footer';
 import Involvement from './components/core/involvement/Involvement';
 import Projects from './components/core/projects/Projects';
+import Prism from './components/misc/background/Prism';
 
 function App() {
   return (
-    <div className="container">
-      <Header />
-      <Introduction />
-      <Experience />
-      <Involvement />
-      <Projects />
-      <Footer />
-    </div>
+    <>
+      <Prism
+        animationType="3drotate"
+        timeScale={0.2}
+        height={7.3}
+        baseWidth={9.7}
+        scale={3}
+        hueShift={0}
+        colorFrequency={1}
+        noise={0.2}
+        glow={1}
+      />
+      <div className="container">
+        <Header />
+        <Introduction />
+        <Experience />
+        <Involvement />
+        <Projects />
+        <Footer />
+      </div>
+    </>
   );
 }
 
